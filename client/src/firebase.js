e9af7f58d989e21a13027d7cc0362ade069a94e3
Ashwani <nishantpandita999@gmail.com>
2024-07-05 10:14:42 +0530

    fixed firebase errors

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: "mern-estate-64d34.firebaseapp.com",
+  projectId: "mern-estate-64d34",
+  storageBucket: "mern-estate-64d34.appspot.com",
+  messagingSenderId: "238593924840",
+  appId: "1:238593924840:web:ffed20eb2aa835d630b35d"
 };
 
 // Initialize Firebase
